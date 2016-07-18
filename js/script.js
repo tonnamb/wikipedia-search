f594793a78309ab8bc5189947c940fa01524e89a
Tonnam Balankura <tonnam.balankura@gmail.com>
2016-07-17 22:32:42 -0400

    put whole code in one closure

--- js/script.js
+++ js/script.js
@@ -5,23 +5,24 @@ $(document).ready(function () {
 
   "use strict";
 
-  function getSearchResults(searchQuery, callback) {
-    $.getJSON("https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=" + searchQuery + "&format=json&callback=?", function (searchResults) {
-      callback(searchResults);
-    });
-  }
-
-  function displaySearchResults(searchResults) {
-    $("#results-box").empty();
-    for (var i=0; i<searchResults.query.search.length; i++) {
-      var $div = $("<div>", {"class": "entry"});
-      $div.append('<h4><a href="https://en.wikipedia.org/wiki/'+searchResults.query.search[i].title+'" target="_blank">'+searchResults.query.search[i].title+'</h4>');
-      $div.append('<p>'+searchResults.query.search[i].snippet+' ...</p>');
-      $("#results-box").append($div);
-    }
-  }
-
   (function () {
+
+    function getSearchResults(searchQuery, callback) {
+      $.getJSON("https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=" + searchQuery + "&format=json&callback=?", function (searchResults) {
+        callback(searchResults);
+      });
+    }
+
+    function displaySearchResults(searchResults) {
+      $("#results-box").empty();
+      for (var i=0; i<searchResults.query.search.length; i++) {
+        var $div = $("<div>", {"class": "entry"});
+        $div.append('<h4><a href="https://en.wikipedia.org/wiki/'+searchResults.query.search[i].title+'" target="_blank">'+searchResults.query.search[i].title+'</h4>');
+        $div.append('<p>'+searchResults.query.search[i].snippet+' ...</p>');
+        $("#results-box").append($div);
+      }
+    }
+
     $("#search").click(function () {
       getSearchResults($('#search-form').val(), displaySearchResults);
     });
